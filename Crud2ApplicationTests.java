080bc6a80de63e7895a55561d7196ccc419067f2
Edward-hub17 <122854151+Edward-hub17@users.noreply.github.com>
2023-04-19 00:50:31 +0300

    Add files via upload

---
+++ Crud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project3.crud2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Crud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
